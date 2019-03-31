586c2c514027624c3346a6b5f74e58972d407155
Prithveesh Goel <prithveeshgoel@Prithveeshs-MacBook-Pro.local>
2019-04-01 01:34:05 +0530

    Added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
-module.exports = {
+/* eslint-disable import/no-extraneous-dependencies */
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+
+const withBundleAnalyzerConfig = {
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: './bundles/server.html',
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: './bundles/client.html',
+    },
+  },
+};
+
+const nextConfig = {
   distDir: '../build/next',
 };
+
+module.exports = withPlugins(
+  [[withBundleAnalyzer, withBundleAnalyzerConfig]],
+  nextConfig,
+);
