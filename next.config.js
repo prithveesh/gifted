36ba7443e2d50a99c5945f497daee0352506d136
Prithveesh Goel <prithveeshgoel@Prithveeshs-MacBook-Pro.local>
2019-04-03 00:28:09 +0530

    config updated

--- next.config.js
+++ next.config.js
@@ -1,29 +1,25 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const withPlugins = require('next-compose-plugins');
-const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 const withSass = require('@zeit/next-sass');
+// const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 
-const withBundleAnalyzerConfig = {
-  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/server.html',
-    },
-    browser: {
-      analyzerMode: 'static',
-      reportFilename: './bundles/client.html',
-    },
-  },
-};
+// const withBundleAnalyzerConfig = {
+//   analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+//   analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+//   bundleAnalyzerConfig: {
+//     server: {
+//       analyzerMode: 'static',
+//       reportFilename: '../bundles/server.html',
+//     },
+//     browser: {
+//       analyzerMode: 'static',
+//       reportFilename: './bundles/client.html',
+//     },
+//   },
+// };
 
 const nextConfig = {
   target: 'serverless',
 };
 
-module.exports = withPlugins(
-  [withSass],
-  [[withBundleAnalyzer, withBundleAnalyzerConfig]],
-  nextConfig,
-);
+module.exports = withPlugins([withSass], nextConfig);
